00c334d4a5ea43e8d40cf7bd5377e7693a91531f
AlbertoDiWeb <a.dieguez.alvarez@gmail.com>
2020-03-15 04:21:33 +0100

    estudiando python

--- helloWorlPython/hello.py
+++ helloWorlPython/hello.py
@@ -1,2 +1,7 @@
-msg = "Hola Mundo"
-print(msg)
+#coding: utf8
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
